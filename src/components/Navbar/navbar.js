3595358088306f600306a9dfb2824fece1fca66b
genti180302 <gentian_1@live.at>
2024-09-12 11:05:36 +0200

    fixes

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -96,7 +96,7 @@ const Navbar = () => {
                     style={{ color: 'white'}}/></a>
                 <a href={'https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile'}><FaLinkedin
                     style={{ color: 'white'}}/></a>
-                <a href={'/Bewerbungsmappe_Latifi_2024._D_.pdf'}>
+                <a href={'/Bewerbungsmappe_Latifi_2024.D_.09.pdf'}>
                     <IoAccessibilitySharp style={{ color: 'white'}}/>
                 </a>
             </div>
