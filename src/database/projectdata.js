fa7b120e568170cf17415490e5397859e53dc557
genti180302 <gentian_1@live.at>
2025-01-26 22:07:49 +0100

    fixes

--- src/database/projectdata.js
+++ src/database/projectdata.js
@@ -1,6 +1,7 @@
 const projectData = [
     {
         project:'Cryptotracker',
+        link: 'https://668aeb69101d2c463cef2223--gegocrypto.netlify.app/',
         github: 'https://github.com/genti180302/Final_Project'
     },
     {
